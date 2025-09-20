d69ca4595ce8aed9c4c4f2a5c250075cc22d007c
KarollJesus <A01669764@tec.mx>
2025-09-20 17:00:37 -0600

    proyecto Tec

--- calcularFig.py
+++ calcularFig.py
@@ -1,3 +1,21 @@
+
 import pandas as pd
+#importar las funciones que calculan el perimetro y area
+#de cada figura
+from figuras import rectangulo, triangulo, circulo
 df = pd.read_csv("figuras.csv")
 print("El archivo ha sido cambiado")
+
+for index, row in df.iterrows():
+	if  row['FIGURA'] == 'c':
+		calculos = circulo(row['MEDIDA1'])
+		print(f"fila{index}, Figura = Circulo, , MEDIDA1 = {row['MEDIDA1']} , MEDIDA2 = {row['MEDIDA2']}, AREA = {calculos[0]}, PERIMETRO = {calculos[1]}")
+	elif row['FIGURA'] == 't':
+		calculos = triangulo(row['MEDIDA1'],row['MEDIDA2'])
+		print(f"fila{index}, Figura = Triangulo, MEDIDA1 = {row['MEDIDA1']} , MEDIDA2 = {row['MEDIDA2']}, AREA = {calculos[0]}, PERIMETRO = {calculos[1]}")
+	else:
+		calculos = rectangulo(row['MEDIDA1'],row['MEDIDA2'])
+		print(f"fila{index}, Figura = Rectangulo,  MEDIDA1 = {row['MEDIDA1']} , MEDIDA2 = {row['MEDIDA2']}, AREA = {calculos[0]}, PERIMETRO = {calculos[1]}")
+	
+
+
--- figuras.py
+++ figuras.py
@@ -1,4 +1,20 @@
+import math #importo la libreria de math
+
+#funciones que devuelven el area y perimetro de un figura
 def rectangulo(base,altura):
 	area = base * altura
-	perimetro 2 * (base + altura)
+	perimetro = 2 * (base + altura)
 	return area, perimetro
+
+def triangulo(base,altura):
+	lado = ((altura**2)+((base/2)**2))**(1/2)
+	area = (base*altura)/2
+	perimetro = base + 2*lado
+	return area,perimetro
+
+def circulo(radio):
+	area = math.pi * (radio**2)
+	perimetro = math.pi *2*radio
+	return area, perimetro
+
+
